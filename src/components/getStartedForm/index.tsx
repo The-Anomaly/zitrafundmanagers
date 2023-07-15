01b6f311ed44954137043e20d31cafb0aa91a168
Favour Adekogbe <favouradekogbe@gmail.com>
2023-07-15 01:53:44 +0100

    update get started form

--- src/components/getStartedForm/index.tsx
+++ src/components/getStartedForm/index.tsx
@@ -68,9 +68,11 @@ const GetStartedFormUI: React.FC<GetStartedProps> = ({ submit, clear }) => {
 
   useEffect(() => {
     reset();
+    document.getElementsByTagName("html")[0].style.overflow = "hidden";
   }, [clear]);
 
   const close = () => {
+    document.getElementsByTagName("html")[0].style.overflow = "unset";
     searchParams.delete("getstarted");
     setSearchParams(searchParams);
   };
@@ -88,130 +90,133 @@ const GetStartedFormUI: React.FC<GetStartedProps> = ({ submit, clear }) => {
     <>
       <aside className={styles.container}>
         <section className={styles.content}>
-          <CloseIcon onClick={close} role="button" className={styles.close} />
-          <h1 className={styles.ttl}>Get started</h1>
-          <p className={styles.txt}>
-            Get enquiry into any of our products, and lets help you build a
-            healthy financial portfolio
-          </p>
-
-          <form className={styles.form}>
-            <div className={`${styles.inputWrap}`}>
-              <label>First name</label>
-              <input
-                type={"text"}
-                placeholder="your first name"
-                {...register("firstName", {
-                  required: true,
-                })}
-              />
-              {errors.firstName?.message ? (
-                <p className={styles.errorMessage}>
-                  {errors.firstName?.message}
-                </p>
-              ) : (
-                ""
-              )}
-            </div>
-            <div className={`${styles.inputWrap}`}>
-              <label>Last name</label>
-              <input
-                type={"text"}
-                placeholder="your last name"
-                {...register("lastName", {
-                  required: true,
-                })}
-              />
-              {errors.lastName?.message ? (
-                <p className={styles.errorMessage}>
-                  {errors.lastName?.message}
-                </p>
-              ) : (
-                ""
-              )}
-            </div>
-            <div className={`${styles.inputWrap}`}>
-              <label>Email</label>
-              <input
-                type={"email"}
-                placeholder="your email"
-                {...register("email", {
-                  required: true,
-                })}
-              />
-              {errors.email?.message ? (
-                <p className={styles.errorMessage}>{errors.email?.message}</p>
-              ) : (
-                ""
-              )}
-            </div>
-            <div className={`${styles.inputWrap}`}>
-              <label>Phone number</label>
-              <div className={styles.selectWrap}>
-                <Select
-                  onChange={(x: any) => setValue("countryCode", x)}
-                  className={`${styles.select}`}
-                  classNamePrefix="formSelect"
-                  name={"countryCode"}
-                  options={countryCodes.map((item) => ({
-                    label: item.dial_code,
-                    value: item.dial_code,
-                  }))}
-                  value={
-                    watch("countryCode").value ? watch("countryCode") : null
-                  }
-                />
+          <div  className={styles.body}>
+            <CloseIcon onClick={close} role="button" className={styles.close} />
+            <h1 className={styles.ttl}>Get started</h1>
+            <p className={styles.txt}>
+              Get enquiry into any of our products, and lets help you build a
+              healthy financial portfolio
+            </p>
+            <form className={styles.form}>
+              <div className={`${styles.inputWrap}`}>
+                <label>First name</label>
                 <input
-                  placeholder=""
-                  type={"email"}
-                  {...register("number", {
+                  type={"text"}
+                  placeholder="your first name"
+                  {...register("firstName", {
                     required: true,
                   })}
                 />
+                {errors.firstName?.message ? (
+                  <p className={styles.errorMessage}>
+                    {errors.firstName?.message}
+                  </p>
+                ) : (
+                  ""
+                )}
               </div>
-              {errors.number?.message ? (
-                <p className={styles.errorMessage}>{errors.number?.message}</p>
-              ) : (
-                ""
-              )}
-            </div>
-            <div className={`${styles.inputWrap}`}>
-              <label>Location</label>
-              <Select
-                onChange={(x: any) => setValue("location", x)}
-                className={`${styles.select}`}
-                classNamePrefix="formSelect"
-                name={"location"}
-                options={locationOptions}
-                value={watch("location").value ? watch("location") : null}
-              />
-              {errors.location?.value?.message ? (
-                <p className={styles.errorMessage}>
-                  {errors.location?.value?.message}
-                </p>
-              ) : (
-                ""
-              )}
-            </div>
-            <div className={`${styles.inputWrap}`}>
-              <label>Choose the kind of service you need</label>
-              <Select
-                onChange={(x: any) => setValue("service", x)}
-                className={`${styles.select}`}
-                classNamePrefix="formSelect"
-                name={"service"}
-                options={serviceOptions}
-                value={watch("service").value ? watch("service") : null}
-              />
-              {errors.service?.value?.message ? (
-                <p className={styles.errorMessage}>
-                  {errors.service?.value?.message}
-                </p>
-              ) : (
-                ""
-              )}
-            </div>
-          </form>
+              <div className={`${styles.inputWrap}`}>
+                <label>Last name</label>
+                <input
+                  type={"text"}
+                  placeholder="your last name"
+                  {...register("lastName", {
+                    required: true,
+                  })}
+                />
+                {errors.lastName?.message ? (
+                  <p className={styles.errorMessage}>
+                    {errors.lastName?.message}
+                  </p>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className={`${styles.inputWrap}`}>
+                <label>Email</label>
+                <input
+                  type={"email"}
+                  placeholder="your email"
+                  {...register("email", {
+                    required: true,
+                  })}
+                />
+                {errors.email?.message ? (
+                  <p className={styles.errorMessage}>{errors.email?.message}</p>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className={`${styles.inputWrap}`}>
+                <label>Phone number</label>
+                <div className={styles.selectWrap}>
+                  <Select
+                    onChange={(x: any) => setValue("countryCode", x)}
+                    className={`${styles.select}`}
+                    classNamePrefix="formSelect"
+                    name={"countryCode"}
+                    options={countryCodes.map((item) => ({
+                      label: item.dial_code,
+                      value: item.dial_code,
+                    }))}
+                    value={
+                      watch("countryCode").value ? watch("countryCode") : null
+                    }
+                  />
+                  <input
+                    placeholder=""
+                    type={"email"}
+                    {...register("number", {
+                      required: true,
+                    })}
+                  />
+                </div>
+                {errors.number?.message ? (
+                  <p className={styles.errorMessage}>
+                    {errors.number?.message}
+                  </p>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className={`${styles.inputWrap}`}>
+                <label>Location</label>
+                <Select
+                  onChange={(x: any) => setValue("location", x)}
+                  className={`${styles.select}`}
+                  classNamePrefix="formSelect"
+                  name={"location"}
+                  options={locationOptions}
+                  value={watch("location").value ? watch("location") : null}
+                />
+                {errors.location?.value?.message ? (
+                  <p className={styles.errorMessage}>
+                    {errors.location?.value?.message}
+                  </p>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className={`${styles.inputWrap}`}>
+                <label>Choose the kind of service you need</label>
+                <Select
+                  onChange={(x: any) => setValue("service", x)}
+                  className={`${styles.select}`}
+                  classNamePrefix="formSelect"
+                  name={"service"}
+                  options={serviceOptions}
+                  value={watch("service").value ? watch("service") : null}
+                />
+                {errors.service?.value?.message ? (
+                  <p className={styles.errorMessage}>
+                    {errors.service?.value?.message}
+                  </p>
+                ) : (
+                  ""
+                )}
+              </div>
+            </form>
+          </div>
           <div className={styles.btnSec}>
             <button onClick={cancel} className={styles.cancel}>
               Cancel
